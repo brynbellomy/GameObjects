01f5ceace1d5d4c979dc68ca54c6683acf3972a1
bryn austin bellomy <bryn@signals.io>
2015-01-29 23:49:33 -0600

    Explosions are useful!

---
+++ Classes/Explosion.swift
@@ -0,0 +1,29 @@
+//
+//  Explosion.swift
+//  GameObjects
+//
+//  Created by bryn austin bellomy on 2015 Jan 28.
+//  Copyright (c) 2015 bryn austin bellomy. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+
+public struct Explosion
+{
+    public let emitter:  SKEmitterNode
+    public let duration: NSTimeInterval
+
+    public init(emitter e:SKEmitterNode, duration d:NSTimeInterval) { //, radius r:CGFloat) {
+        emitter = e
+        duration = d
+    }
+
+    public func explode() {
+        emitter.zPosition = 999
+        emitter.runOnceForDuration(duration)
+    }
+}
+
+
